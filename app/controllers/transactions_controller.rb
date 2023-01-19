dda0a0b0dc5d86e3f2bd62db9cfdcdf465904f38
Max Tan <yenseow@gmail.com>
2023-01-19 11:47:31 -0500

    removed extraneous routes / code

--- app/controllers/transactions_controller.rb
+++ app/controllers/transactions_controller.rb
@@ -1,40 +1,24 @@
 class TransactionsController < ApplicationController
-  before_action :set_transaction, only: %i[ show edit update destroy ]
+  before_action :set_transaction, only: %i[ edit update destroy ]
 
-  # GET /transactions or /transactions.json
-  def index
-    @transactions = Transaction.all
-  end
-
-  # GET /transactions/1 or /transactions/1.json
-  def show
-  end
-
-  # GET /transactions/new
-  def new
-    @transaction = Transaction.new
-  end
-
-  # GET /transactions/1/edit
-  def edit
-  end
-
-  # POST /transactions or /transactions.json
+  # POST /holdings/1/transactions, prefix: holding_transactions
   def create
     @transaction = Transaction.new(transaction_params)
 
     respond_to do |format|
       if @transaction.save
         format.html { redirect_to transaction_url(@transaction), notice: "Transaction was successfully created." }
-        format.json { render :show, status: :created, location: @transaction }
       else
         format.html { render :new, status: :unprocessable_entity }
-        format.json { render json: @transaction.errors, status: :unprocessable_entity }
       end
     end
   end
 
-  # PATCH/PUT /transactions/1 or /transactions/1.json
+  # GET /transactions/1/edit, prefix: edit_transaction
+  def edit
+  end
+
+  # PATCH/PUT /transactions/1
   def update
     respond_to do |format|
       if @transaction.update(transaction_params)
@@ -47,13 +31,12 @@ class TransactionsController < ApplicationController
     end
   end
 
-  # DELETE /transactions/1 or /transactions/1.json
+  # DELETE /transactions/1, prefix: transaction
   def destroy
     @transaction.destroy
 
     respond_to do |format|
       format.html { redirect_to transactions_url, notice: "Transaction was successfully destroyed." }
-      format.json { head :no_content }
     end
   end
 
@@ -65,6 +48,6 @@ class TransactionsController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def transaction_params
-      params.require(:transaction).permit(:price, :quantity, :holding_id)
+      params.require(:transaction).permit(:price, :quantity)
     end
 end
