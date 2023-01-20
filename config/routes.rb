1bd950cfd517f8495a5decfc4169bb42cc3c64f6
Max Tan <yenseow@gmail.com>
2023-01-20 13:54:48 -0500

    new route to add a transaction

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,8 @@ Rails.application.routes.draw do
   root "portfolios#index"
 
   resources :portfolios do
-    resources :holdings, only: [:new, :show, :create, :destroy], shallow: true do
-      resources :transactions, except: [:new, :index, :show], shallow: true
+    resources :holdings, except: [:index, :edit, :update], shallow: true do
+      resources :transactions, except: [:index, :show], shallow: true
     end
   end
 end
