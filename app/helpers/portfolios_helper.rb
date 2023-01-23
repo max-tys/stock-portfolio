99c0874c7ab25bdade2e4766ef02997e630b0d76
Max Tan <yenseow@gmail.com>
2023-01-23 14:11:36 -0500

    calculate delta

--- app/helpers/portfolios_helper.rb
+++ app/helpers/portfolios_helper.rb
@@ -1,16 +1,30 @@
 module PortfoliosHelper
   def get_portfolio_attributes(portfolio)
+    amount_invested = get_amount_invested(portfolio)
+    current_value = get_portfolio_current_value(portfolio)
+    delta = get_delta(amount_invested, current_value)
+
     {
       name: portfolio.name,
-      amount_invested: "$ #{number_format(get_amount_invested(portfolio))}",
-      current_value: "$ #{number_format(0)}",
-      delta: "#{number_format(0)} %"
+      amount_invested: "$ #{number_format(amount_invested)}",
+      current_value: "$ #{number_format(current_value)}",
+      delta: "#{number_format(delta)} %"
     }
   end
 
-  private
-
   def get_amount_invested(portfolio)
     portfolio.amount_invested.nil? ? 0 : portfolio.amount_invested
+  end
+
+  def get_portfolio_current_value(portfolio)
+    portfolio.current_value.nil? ? 0 : portfolio.current_value
+  end
+
+  def get_delta(amount_invested, current_value)
+    begin
+      100 * (current_value - amount_invested) / amount_invested
+    rescue ZeroDivisionError
+      0
+    end
   end
 end
