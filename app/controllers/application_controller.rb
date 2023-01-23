e97209bcf6b7a10f709a4dfeecc79e1e11f97463
Max Tan <yenseow@gmail.com>
2023-01-23 14:10:24 -0500

    contain finnhub helper within app controller class

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,7 +5,17 @@ class ApplicationController < ActionController::Base
     config.api_key['api_key'] = Rails.application.credentials.dig(:finnhub, :api_key)
   end
 
-  def self.finnhub_client
+  def finnhub_client
     FinnhubRuby::DefaultApi.new
   end
+
+  def get_last_price(holding)
+    begin
+      finnhub_client.quote(holding.symbol).c
+    rescue FinnhubRuby::ApiError
+      '-'
+    end
+  end
+  
+  helper_method :get_last_price
 end
