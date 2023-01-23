10b0b5f5864abc0f7adec896af9a7752572c8cee
Max Tan <yenseow@gmail.com>
2023-01-23 14:11:54 -0500

    add current value to holding model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_01_18_172008) do
+ActiveRecord::Schema[7.0].define(version: 2023_01_23_182028) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -19,6 +19,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_01_18_172008) do
     t.bigint "portfolio_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.decimal "last_price"
     t.index ["portfolio_id"], name: "index_holdings_on_portfolio_id"
   end
 
