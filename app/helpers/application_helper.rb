37fb2d69c8212903cc6f244ea28b279582124419
Max Tan <yenseow@gmail.com>
2023-01-23 11:48:13 -0500

    add view helpers to format number

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,5 @@
 module ApplicationHelper
+  def number_format(number)
+    number_with_precision(number, :precision => 2, :delimiter => ',')
+  end
 end
--- app/helpers/holdings_helper.rb
+++ app/helpers/holdings_helper.rb
@@ -1,2 +1,18 @@
 module HoldingsHelper
+  def last_price(symbol)
+    begin
+      ApplicationController.finnhub_client.quote(symbol).c
+    rescue FinnhubRuby::ApiError
+      '-'
+    end
+  end
+
+  def current_value(quantity, price)
+    begin
+      number_format(quantity * price)
+    # Rescue if there are no transactions for a given holding
+    rescue NoMethodError
+      '-'
+    end
+  end
 end
