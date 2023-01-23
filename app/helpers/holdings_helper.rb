81feec0241954cfed797138ae5c56663e57426bf
Max Tan <yenseow@gmail.com>
2023-01-23 14:11:15 -0500

    move finnhub helper to app controller

--- app/helpers/holdings_helper.rb
+++ app/helpers/holdings_helper.rb
@@ -11,22 +11,12 @@ module HoldingsHelper
     }
   end
 
-  private
-
   def get_quantity(holding)
     holding.quantity.nil? ? 0 : holding.quantity
   end
 
   def get_average_cost(holding)
     holding.average_cost.nil? ? 0 : holding.average_cost
-  end
-
-  def get_last_price(holding)
-    begin
-      ApplicationController.finnhub_client.quote(holding.symbol).c
-    rescue FinnhubRuby::ApiError
-      '-'
-    end
   end
 
   def get_current_value(quantity, last_price)
