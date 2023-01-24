55057458fca6804d73a3e045297050ba5ed933b9
Max Tan <yenseow@gmail.com>
2023-01-24 10:03:11 -0500

    rubocop

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
-  root "portfolios#index"
+  root 'portfolios#index'
 
   resources :portfolios do
-    resources :holdings, except: [:index, :edit, :update], shallow: true do
-      resources :transactions, except: [:index, :show], shallow: true
+    resources :holdings, except: %i[index edit update], shallow: true do
+      resources :transactions, except: %i[index show], shallow: true
     end
   end
 end
